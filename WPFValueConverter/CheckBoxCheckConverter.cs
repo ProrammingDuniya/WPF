e5a433e8ab06c33c500981991003f7f7499ec1d4
Abhishek <Abhishek@DESKTOP-RONAPNL>
2023-01-29 17:55:57 +0530

    WPF Converter

---
+++ WPFValueConverter/CheckBoxCheckConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPFValueConverter
+{
+    /// <summary>
+    /// Summary description for Class1
+    /// </summary>
+    public class CheckBoxCheckConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value?.ToString()?.ToUpper() == "MARRIED")
+                return true;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool married = System.Convert.ToBoolean(value);
+            if (married == true)
+                return "Married";
+            else
+                return "Unmarried";
+        }
+    }
+}
---
+++ WPFValueConverter/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPFValueConverter1
+{
+    internal class Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value?.ToString()?.ToLower() == "married")
+                return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool data = System.Convert.ToBoolean(value);
+            if (data)
+                return "Married";
+
+            return "UnMarried";
+        }
+    }
+}
